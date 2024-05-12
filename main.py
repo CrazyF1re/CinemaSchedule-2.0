ee279ff6ff00dd3a2f3985efe160556b527dfabe
CrazyF1re <vova.plisko@mail.ru>
2024-05-12 17:27:36 +0700

    start version of template of page with film.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import create_engine, MetaData
-
+from urllib.parse import unquote
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
@@ -16,16 +17,55 @@ def get_main(request: Request):
     meta = MetaData()
     meta.reflect(engine)
     film_table = meta.tables['film_table']
-    sessions_table = meta.tables['sessions_table']
     
     if (request.cookies.get('cinemas')):
         cinemas = [int(i) for i in request.cookies.get('cinemas').split(',')]
         with engine.connect() as connection:
-            films = connection.execute(film_table.select().group_by(film_table.c.name).where(film_table.c.img_url !='' and film_table.c.id_cinema.in_(cinemas)).with_only_columns(film_table.c.name,film_table.c.img_url,film_table.c.url)).fetchall()
+            films = connection.execute(film_table.select().group_by(film_table.c.name)
+                                       .where(film_table.c.img_url !='')
+                                       .where(film_table.c.cinema_id.in_(cinemas))
+                                       .with_only_columns(film_table.c.name,film_table.c.img_url)).fetchall()
     else:
         with engine.connect() as connection:
-            films = connection.execute(film_table.select().group_by(film_table.c.name).where(film_table.c.img_url !='').with_only_columns(film_table.c.name,film_table.c.img_url,film_table.c.url)).fetchall()     
-    
-    
+            films = connection.execute(film_table.select().group_by(film_table.c.name)
+                                       .where(film_table.c.img_url !='')
+                                       .with_only_columns(film_table.c.name,film_table.c.img_url)).fetchall()     
     
     return templates.TemplateResponse(request=request, name='main.html',context={"films":films})
+
+@app.get('/films/{film}/')
+def get_film(request:Request,film):
+    if(request.cookies.get('cinemas')):
+        cinemas = [int(i) for i in request.cookies.get('cinemas').split(',')]
+    else:
+        cinemas = [1,2,3,4,5]
+    engine = create_engine('sqlite:///database.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    meta = MetaData()
+    meta.reflect(engine)
+    film_table = meta.tables['film_table']
+    sessions_table = meta.tables['sessions_table']
+
+    with engine.connect() as connection:
+        film_id = connection.execute(film_table.select()
+                                    .where(film_table.c.name == unquote(film))
+                                    .where(film_table.c.cinema_id.in_(cinemas))
+                                    .with_only_columns(film_table.c.id)).fetchall()
+        film_id = [item[0]  for item in film_id]
+        print(film_id,cinemas)
+        all_sessions = []
+        all_sessions.append(unquote(film))
+        all_sessions.append(connection.execute(film_table.select()
+                                    .where(film_table.c.name == unquote(film))
+                                    .with_only_columns(film_table.c.img_url))
+                                    .fetchone()[0])
+        all_sessions.append([])
+        for id,cinema in zip(film_id,cinemas):   
+            sessions =connection.execute(sessions_table.select()
+                                        .where(sessions_table.c.film_id == id)
+                                        .with_only_columns(sessions_table.c.datetime,sessions_table.c.price)).fetchall()
+            all_sessions[2].append({cinema:sessions})
+    print(all_sessions)
+    return templates.TemplateResponse(request=request, name='film.html',context={"info":all_sessions})
+    
